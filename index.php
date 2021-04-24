3d009fac6c3454c60323c9a1e4c1f56b92dcda3d
linnykerxd9 <linnykerxd9@gmail.com>
2021-04-24 16:06:12 -0300

    Meu index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$categorias  = [];
+$categorias[] = "infantil";
+$categorias[] = "adolescente";
+$categorias[] = "adulto";
+
+//print_r($categorias);
+
+$nome = "Jonas";
+$idade = 16;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12){
+    echo " O nadador ",$nome, " compete na categoria:" ,$categorias[0];
+}
+else if($idade >=13 && $idade <= 18){
+    echo "O nadador ",$nome, " compete na categoria:" ,$categorias[1];
+}
+else {
+    echo "O nadador ",$nome, " compete na categoria:" ,$categorias[2];
+}
